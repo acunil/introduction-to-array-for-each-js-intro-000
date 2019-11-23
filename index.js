d3654467293b1424cd7782c8e05707d88ac784a2
Lucian Amos <locamos@gmail.com>
2019-11-23 18:06:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ var cart = [
   {"name":"bike", "price":3}
   ];
   
+
+function checkCart(item)
+cart.forEach(obj => {
+  if(obj.name === item){
+    return `${item} costs $${obj.price}.`;
+  } else {
+    return `${item} is not sold here.`;
+  }
+})
